9ad1f95f510a663549520b6a561362c3826a9d65
SeidM27 <seidmamuti1313@gmail.com>
2026-05-10 12:52:34 +0200

    Update length.c
    seid mamuti assignment 2

--- github-education-c-exercises/03-string-length/length.c
+++ github-education-c-exercises/03-string-length/length.c
@@ -15,3 +15,7 @@ int string_length(string s)
     // TODO: count chars until '\0'
     return 0;
 }
+#include <cs50.h>
+#include <stdio.h>
+int string_length(string s);
+int main(void){string name=get_string("Name: ");int len=string_length(name);printf("%i\n",len);}
